4da3b0a448272c4890b921474fd92bc4bad8df91
rjeby <yassine442@gmail.com>
2026-08-30 19:32:33 +0100

    initialize Spring Boot backend

---
+++ java-backend/src/main/java/com/rjeby/i2note/I2noteApplication.java
@@ -0,0 +1,13 @@
+package com.rjeby.i2note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I2noteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I2noteApplication.class, args);
+	}
+
+}
---
+++ java-backend/src/test/java/com/rjeby/i2note/I2noteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjeby.i2note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I2noteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
